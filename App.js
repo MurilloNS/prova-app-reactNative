ce9e3e6a66f7b8c4d8324d5b1fb0ea02f2628333
MurilloNS <murillo.murillo2001@gmail.com>
2022-07-21 14:45:54 -0300

    Navbar

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
-import { StyleSheet, SafeAreaView } from 'react-native';
+import React from 'react';
+import { StyleSheet, SafeAreaView, StatusBar } from 'react-native';
+import Title from './src/Title/';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      
+    <SafeAreaView style={styles.container}>
+      <Title/>
     </SafeAreaView>
   );
 }
@@ -11,8 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#FFF',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
 });
--- src/Title/index.js
+++ src/Title/index.js
@@ -1,7 +1,31 @@
 import React from "react";
-import {View, Text} from "react-native";
+import { View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 import styles from "./styles";
 
-export default function Title(){
-    
+import HouseSvg from "../../img/icons/House.svg";
+import LupaSvg from "../../img/icons/Lupa.svg";
+import LogoSvg from "../../img/logo.svg";
+
+export default function Title() {
+  return (
+    <View style={styles.navbar}>
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 0 }}
+        colors={["#048C34", "#FFF", "#048C34"]}
+        style={styles.gradient}
+      >
+        <View style={styles.containersHouseLupa}>
+          <HouseSvg style={styles.svgs} />
+        </View>
+        <View style={styles.containerLogo}>
+          <LogoSvg style={styles.svgs} />
+        </View>
+        <View style={styles.containersHouseLupa}>
+          <LupaSvg style={styles.svgs}></LupaSvg>
+        </View>
+      </LinearGradient>
+    </View>
+  );
 }
