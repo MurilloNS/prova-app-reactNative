cab5336b2ea39d8860855ff35ebf9930646eda3d
Daniel C <daniel.campos@gmx.us>
2022-07-29 11:12:48 -0300

    added default page format

--- src/views/BerthedShips/index.js
+++ src/views/BerthedShips/index.js
@@ -1,10 +1,35 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, ImageBackground, Text } from "react-native";
+
+import styles from "./styles";
+
+import LogoSvg from "../../../img/logo.svg";
+import BerthedShipsSvg from "../../../img/icons/BerthedShips.svg";
 
 export default function BerthedShips(){
-    return(
+    return (
         <View>
-            <Text>Navios Atracados</Text>
+          <ImageBackground
+            style={styles.photoHomepage}
+            source={require("../../../img/PhotoHomepage.jpg")}
+          >
+            <View style={styles.boxLogoSvg}>
+              <LogoSvg />
+            </View>
+            <View style={styles.boxBerthedShipSvg}>
+              <BerthedShipsSvg style={styles.berthedShipSvg} />
+            </View>
+            <View style={styles.boxText}>
+              <Text style={styles.text}>Navios</Text>
+              <Text style={styles.text}>Atracados</Text>
+            </View>
+          </ImageBackground>
+          <View style={styles.mainOptions} />
         </View>
-    )
+      );
 }
+
+
+
+
+
---
+++ src/views/BerthedShips/styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    photoHomepage: {
+        height: 149,
+        width: 411,
+    },
+    mainOptions: {
+        borderTopColor: "#04ADBF",
+        borderTopWidth: 3,
+        marginBottom: 15,
+    },
+    boxLogoSvg : {
+        height: "40%",
+        width: "30%",
+        marginLeft: 20,
+    },
+    boxBerthedShipSvg: {
+        height: "50%",
+        width: "20%",
+        marginLeft: "10%",
+        flexDirection: "row",
+    },
+    berthedShipSvg: {
+        height: "110%",
+        width: "110%",
+    },
+    textBerthedShip: {
+        color: "#FFFFFF",
+        fontSize: 40,
+    },
+    boxText: {
+        marginLeft: "40%",
+        marginTop: "-20%",
+      },
+    text: {
+        fontSize: 31,
+        color: "#FFFFFF",
+        marginLeft: "-45%",
+        textShadowRadius: 15,
+        textShadowColor: "#000",
+        textShadowOffset: { width: 1, height: 1 },
+        textAlign: "center",
+      },
+});
+export default styles;
