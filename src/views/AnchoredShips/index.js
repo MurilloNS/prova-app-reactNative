b4f9bc7ccd3c98717d9d15eb490874ca48429d8a
JovannyLeite <jovanny.leite@gmail.com>
2022-07-29 16:19:32 -0300

    Added Card

--- src/views/AnchoredShips/index.js
+++ src/views/AnchoredShips/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { View, ImageBackground, Text } from "react-native";
+import { View, ImageBackground, Text, TouchableOpacity } from "react-native";
 
 import styles from "./styles";
-import stylesImg from "../../views/Home/styles";
+
 
 import LogoSvg from "../../../img/logo.svg"
 import AnchorSvg from "../../../img/icons/Anchor.svg"
+import { Button } from "react-native-web";
 
 export default function AnchoredShips() {
     return (
@@ -24,12 +25,39 @@ export default function AnchoredShips() {
                         <Text style={styles.text}>Navios</Text>
                         <Text style={styles.text}>Fundeados</Text>
                     </View>
-                
-
-
-
             </ImageBackground>
-            <View style={stylesImg.mainOptions} />
+                <View style={styles.mainOptions} />
+
+            <View style={styles.container}>
+                 <View style={styles.containerCardInfo}>
+                    
+                    <View style={styles.boxSvg}/>
+                    <View style={styles.textsInfo}>   
+                    <Text style={styles.cardTextLocal}>Local</Text>
+                    <Text style={styles.cardTextName}>Nome do Navio</Text> 
+                    <Text style={styles.cardTextCargoType}>Tipo de Carga</Text>  
+                       
+                        <View style={styles.cardTextCargo}>
+                            <Text>Carga</Text>
+                            <Text>Descarga</Text>
+                        </View> 
+                        <View style={styles.cardTextCargoValue}>
+                            <Text>00000</Text>
+                            <Text style={styles.valueUnload}>00000</Text>
+                        </View> 
+
+                        <View style={styles.buttonDetails}>
+                        <TouchableOpacity style={styles.Button}>
+                            <Text style={styles.textButton}>Detalhes</Text>
+                        </TouchableOpacity>
+                        </View>
+                        </View>
+                        
+                    
+                 </View>
+
+                 
+            </View>
         </View>
     )
 }
--- src/views/AnchoredShips/styles.js
+++ src/views/AnchoredShips/styles.js
@@ -7,6 +7,11 @@ const style = StyleSheet.create({
         width: 411,
         height: 149,
     },
+    mainOptions:{
+        borderTopColor:"#04ADBF",
+        borderTopWidth: 3,
+        marginBottom: 15,
+    },
     boxLogoSvg:{
         height: "40%",
         width: "30%",
@@ -34,12 +39,96 @@ const style = StyleSheet.create({
     text:{
         fontSize: 31,
         color: "#FFFFFF",
-        marginLeft: "-10%",
+        marginLeft: "-45%",
         textShadowRadius: 15,
         textShadowColor: "#000000",
-        texShadowOffset: { width: 1, height: 1 },
+        textShadowOffset: { width: 1, height: 1 },
         
 
     },
+    container:{
+        height: "auto",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    containerCardInfo:{
+        backgroundColor: "#FFFFFF",
+        height: 195,
+        width: "90%",
+        borderTopRightRadius: 10,
+        borderTopLeftRadius: 10,
+        flexDirection: "row",
+        shadowColor: "#000000",
+        shadowOffset: { width: 0, height: 1 },
+        shadowOpacity: 0.2,
+        elevation: 4,
+        marginTop: 10,
+        
+        
+    },
+    boxSvg:{
+        backgroundColor: "#264775",
+        height: "100%",
+        width: "3%",
+        borderTopLeftRadius: 10,
+        
+    },
+    cardTextLocal:{
+        height: 28,
+        width: "40%",
+        fontSize: 15,
+        marginLeft: 12,
+        marginTop: 10,
+        color: "#264775",
+        fontWeight: "bold",
+    },
+    cardTextName:{
+        height: 28,
+        width: "65%",
+        fontSize: 20,
+        marginLeft: 12,
+        fontWeight: "bold",
+        color: "#393939",
+    },
+    cardTextCargoType:{
+        height: 28,
+        width: "65%",
+        fontSize: 15,
+        marginLeft: 12,
+        marginTop: 10,
+        color: "#393939",
+    },
+    cardTextCargo:{
+
+        width: "65%",
+        flexDirection: "row",
+        marginLeft: 45,
+        justifyContent: "space-between",
+    },
+    cardTextCargoValue:{
+        width: "65%",
+        flexDirection: "row",
+        marginLeft: 45,
+        justifyContent: "space-between",
+    },
+    valueUnload:{
+        marginRight: 20,
+    },
+    buttonDetails:{
+        marginTop: 10,
+        flexDirection:"row",
+        justifyContent: "center",
+        alignItems: "center",
+        marginLeft: "12%",
+        height: 27,
+        width: "60%",
+        backgroundColor: "#264775",
+        borderRadius: 10,
+
+    },
+    textButton:{
+        color: "#FFFFFF",
+    }
+    
 });
 export default style;
