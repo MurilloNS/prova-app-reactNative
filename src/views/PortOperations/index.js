3048afb661f5815d30ab4483966b601bfc6a7490
MurilloNS <murillo.murillo2001@gmail.com>
2022-08-08 16:33:25 -0300

    Links Port Operations

--- src/views/PortOperations/index.js
+++ src/views/PortOperations/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { View, Text, ImageBackground, TouchableOpacity } from "react-native";
+import {
+  View,
+  Text,
+  ImageBackground,
+  TouchableOpacity,
+  Linking,
+} from "react-native";
 import Routes from "../../routes";
 
 import styles from "./styles";
@@ -13,7 +19,6 @@ import CompassSvg from "../../../img/icons/Compass.svg";
 
 import LogoSvg from "../../../img/logo.svg";
 
-
 export default function PortOperations() {
   return (
     <View>
@@ -25,13 +30,13 @@ export default function PortOperations() {
           <LogoSvg />
         </View>
         <View style={styles.boxTitle}>
-        <View style={styles.boxShipSvg}>
-          <ShipSvg style={styles.shipSvg} />
-        </View>
-        <View style={styles.boxTextShips}>
-          <Text style={styles.textShip}>Operações</Text>
-          <Text style={styles.textShip}>Portuárias</Text>
-        </View>
+          <View style={styles.boxShipSvg}>
+            <ShipSvg style={styles.shipSvg} />
+          </View>
+          <View style={styles.boxTextShips}>
+            <Text style={styles.textShip}>Operações</Text>
+            <Text style={styles.textShip}>Portuárias</Text>
+          </View>
         </View>
       </ImageBackground>
 
@@ -42,7 +47,14 @@ export default function PortOperations() {
           <View style={styles.firstBoxSvg}>
             <CompassSvg style={styles.iconSvg} />
           </View>
-          <TouchableOpacity style={styles.boxText} onPress={() => navigation.navigate("PortOperations")}>
+          <TouchableOpacity
+            style={styles.boxText}
+            onPress={() =>
+              Linking.openURL(
+                "https://www.portodesantos.com.br/informacoes-operacionais/operacoes-portuarias/navegacao-e-movimento-de-navios/"
+              )
+            }
+          >
             <Text style={styles.textListOptions}>Navegação e Movimento</Text>
             <Text style={styles.text}>Navegação e Movimentação de navios</Text>
           </TouchableOpacity>
@@ -51,40 +63,74 @@ export default function PortOperations() {
           <View style={styles.boxSvg}>
             <CaladoSvg style={styles.iconSvg} />
           </View>
-          <View style={styles.boxText}>
+          <TouchableOpacity
+            style={styles.boxText}
+            onPress={() =>
+              Linking.openURL(
+                "https://www.portodesantos.com.br/informacoes-operacionais/operacoes-portuarias/calados-operacionais-dos-bercos-de-atracacao/"
+              )
+            }
+          >
             <Text style={styles.textListOptions}>Calado Máximo</Text>
             <Text style={styles.text}>Calado Máximo Operacional</Text>
-          </View>
+          </TouchableOpacity>
         </View>
         <View style={styles.containerListOptions}>
           <View style={styles.boxSvg}>
             <PranchaSvg style={styles.iconSvg} />
           </View>
-          <View style={styles.boxText}>
-            <Text style={styles.textListOptions}>Prancha Operacional</Text>
+          <TouchableOpacity style={styles.boxText}>
+            <Text
+              style={styles.textListOptions}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.portodesantos.com.br/informacoes-operacionais/operacoes-portuarias/pranchas-minimas-operacionais/"
+                )
+              }
+            >
+              Prancha Operacional
+            </Text>
             <Text style={styles.text}>Prancha Operacional</Text>
-          </View>
+          </TouchableOpacity>
         </View>
         <View style={styles.containerListOptions}>
           <View style={styles.boxSvg}>
             <BercosSvg style={styles.iconSvg} />
           </View>
-          <View style={styles.boxText}>
-            <Text style={styles.textListOptions}>Berços de Atracação</Text>
+          <TouchableOpacity style={styles.boxText}>
+            <Text
+              style={styles.textListOptions}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.portodesantos.com.br/informacoes-operacionais/operacoes-portuarias/bercos-de-atracacao/"
+                )
+              }
+            >
+              Berços de Atracação
+            </Text>
             <Text style={styles.text}>Berços de Atracação</Text>
-          </View>
+          </TouchableOpacity>
         </View>
         <View style={styles.lastContainerListOptions}>
           <View style={styles.lastBoxSvg}>
             <FundeioSvg style={styles.iconSvg} />
           </View>
-          <View style={styles.boxText}>
-            <Text style={styles.textListOptions}>Áreas de Fundeio</Text>
+          <TouchableOpacity style={styles.boxText}>
+            <Text
+              style={styles.textListOptions}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.portodesantos.com.br/informacoes-operacionais/operacoes-portuarias/areas-de-fundeio/"
+                )
+              }
+            >
+              Áreas de Fundeio
+            </Text>
             <Text style={styles.text}>Áreas de Fundeio</Text>
-          </View>
+          </TouchableOpacity>
         </View>
       </View>
       <SocialMediaSvgs />
     </View>
-  )
+  );
 }
